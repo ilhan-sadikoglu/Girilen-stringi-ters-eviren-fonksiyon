f3abe44b04a4189f29ffa415307fafab867d562f
ilhan-sadikoglu <56480436+ilhan-sadikoglu@users.noreply.github.com>
2020-04-29 11:05:21 +0300

    Update Kaynak.cpp

--- Kaynak.cpp
+++ Kaynak.cpp
@@ -2,27 +2,23 @@
 
 using namespace std;
 
-void TerstrenYaz(const char* str,int i) {
-    if (str[i] == '\0')
-        return;
-    else {
-        TerstrenYaz(str,i+1);
-        cout << str[i];
-    }
-}
 void TerstrenYaz(const char* str) {
-    if (str[0] == '\0')
-        return;
+    if (str[0] == '\0') //base case null olma durumudur.
+        return; // son karaktere yani null a gelince kesilir
     else {
-        TerstrenYaz(str+ 1);
+        TerstrenYaz(str+ 1); //bu şekilde son karaktere kadar gitti. Null görünce kesildi
+        //kesildikten sonra geri geri gelmeye başladı. Önce son karakteri yazdı, sonra bir öncekini
+
         cout << str[0];
     }
 }
+
 int main()
 {
-   const  char* s = "ilfursa";
-   int i;
-    TerstrenYaz(s,0);
-    cout << endl;
-    TerstrenYaz(s);
+    char s[] = "ornek";
+   
+    //Bu bir char dizisi 
+   //dizinin adı aslında ilk adresi gösteren bir pointerdı
+   //buradaki s de aslında dininin hem ilk adresi hem de adı
+    TerstrenYaz(s); //diziyi yolladık
 }
